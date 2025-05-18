0fd43e44794460e47649f9a52876ca5e29366973
kihaas <152589310+kihaas@users.noreply.github.com>
2025-05-18 23:08:49 +0400

    Add files via upload

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-os.getenv("DATABASE_URL")
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 
